afae18210d98aa7279edd44e9fa1b48b17519f62
SergeyGSA <77gsa88gmail.com>
2023-03-30 14:40:01 +0300

    replaced png with webp

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero: React.FC = () => {
       <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-16 w-full flex justify-center items-center">
-        <a href="#about">
+        <a href="#about" aria-label="Section about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{ y: [0, 24, 0] }}
